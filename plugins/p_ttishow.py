e8165715b02e1944b902e2edd193bd0f82915060
alexyks <107933651+alexyks@users.noreply.github.com>
2022-06-28 16:58:09 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [
             [
-                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/mh_world')
+                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/ccg_ott')
             ],
             [
                 InlineKeyboardButton('♻️ HΞLᎮ ♻️', url=f"https://t.me/{temp.U_NAME}?start=help")
